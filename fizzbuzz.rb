bc043f707bd3ff93a233d078ebd58ed58e72a74d
Alison Williams <apw5011@gmail.com>
2020-10-29 02:41:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,13 +3,10 @@
 def fizzbuzz(int)
   if int % 3 == 0
     puts "Fizz"
-  end
-  if int % 5 == 0
+  elsif int % 5 == 0
     puts "Buzz"
-  end
-  if int % 3 == 0 && int % 5 == 0
+  elsif int % 3 == 0 && int % 5 == 0
     puts "FizzBuzz"
-  end
 end
 
 fizzbuzz(15)
